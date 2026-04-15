e7a041f4e804609f281bbe90e472ca7143648670
rafifakbar <mrafif4525125@univpancasila.ac.id>
2026-04-15 13:22:31 +0700

    Add files via upload

---
+++ prakasd2-2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+struct mahasiswa{
+char npm[9];
+char nama[30];
+float ipk;
+
+};
+
+int main (){
+
+mahasiswa mhs ;
+cout << "no pokok mahasiswa = "; cin >> mhs.npm;
+cout << "nama mahasiswa = "; cin >> mhs.nama;
+cout << "IPK  = "; cin >> mhs.ipk;
+cout << endl;
+cout << "=======================================" <<  endl;
+cout<< " DATA ANDA " <<endl;
+cout << "=======================================" <<  endl;
+cout << "no pokok mahasiswa = "<< mhs.npm << endl;
+cout << "nama mahasiswa = "<< mhs.nama << endl;
+cout << "IPK = "<< mhs.ipk << endl;
+cin.get();
+}
